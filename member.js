d174544ef76fe0d064dcc98f8d8ec5502cd233f1
KavyamSachdeva <124245507+Kavyam172@users.noreply.github.com>
2023-09-30 14:38:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    member.style.color = "black";
+    skills.style.color = "white";
+    memberList.style.display = "block";
+    skillsList.style.display = "none";
+}
